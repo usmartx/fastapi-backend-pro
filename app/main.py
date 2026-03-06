23f8226cf1436c5b98aaff85a3df33bd31cdaaf9
Oussama Dahmani <dahmusroussama@gmail.com>
2026-03-06 04:46:36 +0000

    Refactor main.py to use user router

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI, HTTPException
-from app.crud.crud import get_users, get_user, create_user
-from app.schemas.schemas import UserCreate
-from app.database.database import init_db
+from app.crud.crud import get_users, get_user, create_user, update_user, delete_user
+from app.schemas.schemas import UserCreate, UserUpdate
+from app.database.database import init_db, populate_users
 
 app = FastAPI(title="Backend Pro API")
 
+# création table
 init_db()
+
+# remplir base avec 1000 users
+populate_users(1000)
 
 @app.get("/users")
 def read_users():
